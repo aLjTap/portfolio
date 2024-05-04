d1ffc1bd2fe3c508eda6ac32611f1dd4d8f364c0
sozefvs <104849681+sozefvs@users.noreply.github.com>
2024-05-04 16:09:02 +0300

    added about and radialgradient removed dark light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React, { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { ThemeProvider, useTheme } from "./components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
+import { ButtonEvents } from "./components/contant";
+import { FaGithub } from "react-icons/fa";
 function App() {
-  const [count, setCount] = useState(0)
+  const { theme, setTheme } = useTheme();
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const themRef = React.useRef<HTMLDivElement>(null);
+  function handleMouseMovement({
+    clientX,
+    clientY,
+    currentTarget,
+  }: React.MouseEvent) {
+    console.log(clientX, clientY, currentTarget);
+    const { left, top } = currentTarget.getBoundingClientRect();
+
+    const x = clientX - left;
+    const y = clientY - top;
+
+    setMousePosition({ x, y });
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      onMouseMove={handleMouseMovement}
+      ref={themRef}
+      style={{
+        backgroundImage: `radial-gradient(circle at ${mousePosition.x}px ${
+          mousePosition.y
+        }px, ${"rgb(15 23 42)"} 1%, transparent 45%)`,
+      }}
+    >
+      <ThemeProvider>
+        <div className="fixed justify-start items-center w-[50%] h-screen  ">
+          <div
+            className={`flex flex-col justify-start items-start w-full p-[10%] bg-${theme}-bg text-${theme}-fg text-[6rem]`}
+          >
+            Ali Tap
+            <div className={`text-lg text-${theme}-fg`}>
+              Computer Engineer Student
+            </div>
+            <div className={`text-lg text-${theme}-fg opacity-50 `}>
+              I am a computer engineer student at the Istanbul Esenyurt
+              University. I am interested in software development and I am
+              trying to improve myself in this field.
+            </div>
+          </div>
+          <div className="flex flex-col justify-start items-start w-full p-[10%] gap-2">
+            {ButtonEvents.map((event) => (
+              <Button
+                className={`flex bg-${theme}-bg text-${theme}-fg hover:scale-[1.1] hover:bg-transparent border-2 border-${theme}-fg hover:border-${theme}-fg transition-all`}
+                key={event}
+                onClick={() => {
+                  console.log(event);
+                }}
+                size={"sm"}
+              >
+                {event}
+              </Button>
+            ))}
+          </div>
+          <FaGithub className="fixed top-[90%] left-[5%]" size={40} />
+        </div>
+        <div className="flex flex-row w-screen h-screen">
+          <div className="flex w-[50%] h-full"></div>
+          <div className="flex w-[50%] h-full">
+            {/* about me */}
+            <div>
+              <div className="justify-start items-start px-[10%] py-[15%] text-lg opacity-50">
+                I first started programming in 2020 while studying Computer and
+                Information Technologies Instructor at Yildiz Technical
+                University in 2020. Basically, I mainly worked on C programming.
+                In 2023, I left Yildiz Technical University and started the
+                Computer Engineering department at Istanbul Esenyurt University.
+                In these 3 years, I have been involved in many project studies.
+                In November 2023, I entered the Starloop team, which carried out
+                slang work on Hyperloop technology, as the Communication team
+                Responsible and I was involved in many slang studies within the
+                team.
+              </div>
+            </div>
+          </div>
+        </div>
+      </ThemeProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
-  prefix: "yes",
   theme: {
     container: {
       center: true,
@@ -17,6 +17,10 @@ module.exports = {
       },
     },
     extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--radial-gradient))",
+      },
+
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -73,5 +77,6 @@ module.exports = {
       },
     },
   },
+  // eslint-disable-next-line no-undef
   plugins: [require("tailwindcss-animate")],
-}
+};
